43ea5cfd097f1b80cf939e9cd70109fa0b87271a
George Thomas <georgetamayothomas@gmail.com>
2013-10-21 21:40:23 +0100

    Add test for double booking students

--- test/models/detention_test.rb
+++ test/models/detention_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class DetentionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot add two detentions at the same time" do
+    detention_one = Detention.new
+    detention_one.date = Date.today
+    detention_one.time = "Afterschool"
+    detention_one.student_id = 1
+
+    assert detention_one.save
+
+    detention_two = Detention.new
+    detention_two.date = Date.today
+    detention_two.time = "Afterschool"
+    detention_two.student_id = 1
+
+    assert !detention_two.save
+
+    detention_two.student_id = 2
+    assert detention_two.save
+
+    detention_two.student_id = 1
+    detention_two.time = "Lunchtime"
+    assert detention_two.save
+
+    detention_two.time = "Afterschool"
+    detention_two.date = Date.tomorrow
+    assert detention_two.save
+  end
 end
