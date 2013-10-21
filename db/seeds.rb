7656941d63a5c21c930ab9a26e7db0eb1a9eff24
George Thomas <georgetamayothomas@gmail.com>
2013-10-21 16:12:52 +0100

    Add test teacher to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Teacher.create([
+  {name: 'Test McTest', email: 'testmctest@testschool.com', group: ''}
+])
