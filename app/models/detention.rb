f596d05000ce4140d0a0a6362a65ed0f42e5c0b4
George Thomas <georgetamayothomas@gmail.com>
2013-10-24 00:41:48 +0100

    Validate status to one of four options

--- app/models/detention.rb
+++ app/models/detention.rb
@@ -5,4 +5,6 @@ class Detention < ActiveRecord::Base
   validates_presence_of :teacher_id, :student_id, :date, :time, :reason,
   		:points, :parent_detention, :status
   validates_uniqueness_of :time, :scope => [:date, :student_id]
+  
+  validates :status, inclusion: {in: %w(set done rescheduled escalated)}
 end
