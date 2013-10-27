f9ea14a5cee76030b7141ae41a48b9b8dd5ff344
George Thomas <georgetamayothomas@gmail.com>
2013-10-27 17:23:26 +0000

    Add upcoming_separated_by_datetime as a class method

--- app/models/detention.rb
+++ app/models/detention.rb
@@ -2,17 +2,18 @@ class Detention < ActiveRecord::Base
   belongs_to :teacher
   belongs_to :student
 
+  DetTime = Struct.new(:date, :time)
+
   validates_presence_of :teacher_id, :student_id, :date, :time, :reason,
   		:points, :parent_detention, :status
   validates_uniqueness_of :time, :scope => [:date, :student_id]
-  
+
   validates :status, inclusion: {in: %w(set done rescheduled escalated)}
 
-  def upcoming_separated_by_datetime
-    DetTime = Struct.new(:date, :time)
+  def self.upcoming_separated_by_datetime
     all_dets = Detention.where("date > ? AND status = ?", Time.now, 'set')
     unique_det_times = all_dets.uniq.pluck(:date, :time).map{ |det| {
       DetTime.new(det[:date], det[:time]) => all_dets.where(date: det[:date], time: det[:time])
     }}
-  end    
+  end
 end
