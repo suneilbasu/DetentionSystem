978f3679c136bd3f056c70e37e999f456a1b0ae8
George Thomas <georgetamayothomas@gmail.com>
2013-12-14 21:39:43 +0000

    Add validation for detention dates should be in future

--- app/models/detention.rb
+++ app/models/detention.rb
@@ -10,8 +10,11 @@ class Detention < ActiveRecord::Base
 
   validates :status, inclusion: {in: %w(set done rescheduled escalated)}
 
+  validate :date_is_in_future?
+
   def self.upcoming_separated_by_datetime
-    all_dets = Detention.where("date > ? AND status = ?", Time.now, 'set').order(date: :asc, time: :desc)
+    all_dets = Detention.where("date > ? AND status = ?", Time.now, 'set')
+        .order(date: :asc, time: :desc)
     unique_det_times = all_dets.uniq.pluck(:date, :time)
     array = Array.new
     unique_det_times.each do |datetime|
@@ -24,4 +27,11 @@ class Detention < ActiveRecord::Base
   def self.pen
     dets = Detention.where(date: nil)
   end
+
+  private
+    def date_is_in_future?
+      if self.date < Date.today
+        errors.add(:date, 'must be in the future')
+      end
+    end
 end
